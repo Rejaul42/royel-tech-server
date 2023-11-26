c6a5413efef42082d791e83203d7c11746d49cbc
Rejaul42 <rejaulkarimraju91@gmail.com>
2023-11-26 08:14:54 -0600

    get product data for specific users

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 
@@ -49,6 +49,12 @@ async function run() {
     })
 
     // add product
+    app.get('/product/:email', async (req, res) =>{
+      const email = req.params.email;
+      const query = { userEmail: email};
+      const result = await productCollection.find(query).toArray();
+      res.send(result)
+    })
 
     app.post('/product', async (req, res) => {
       const item = req.body;
@@ -56,7 +62,13 @@ async function run() {
       res.send(result);
     });
 
-    
+    app.delete('/product/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await productCollection.deleteOne(query);
+      res.send(result);
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
